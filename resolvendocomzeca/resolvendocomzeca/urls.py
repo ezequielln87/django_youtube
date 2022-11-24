8e11c0dbe19777658a79d19c56274670488a0ad5
ezequielln87 <ezequielln87@gmail.com>
2022-11-24 00:08:20 -0300

    episodio31

--- resolvendocomzeca/resolvendocomzeca/urls.py
+++ resolvendocomzeca/resolvendocomzeca/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenVerifyView
 
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -26,7 +28,10 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("cliente.urls")),
+    path("", include("usuario.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
